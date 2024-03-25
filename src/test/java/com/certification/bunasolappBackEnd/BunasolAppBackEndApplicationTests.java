f2fda464b450c9ad96b3a3223a5a8032f3019b1f
botere <botereaimesome@gmail.com>
2024-03-25 17:48:16 +0000

    Premier commit : Ajout des fichiers initiaux

---
+++ src/test/java/com/certification/bunasolappBackEnd/BunasolAppBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certification.bunasolappBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunasolAppBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
